ceb91678578c7f65de57eb9d69aea789b7f83078
runtu <huorunquan@126.com>
2018-10-09 15:05:33 +0800

    IMP: temporarily disabled csrf check

--- config/config.default.js
+++ config/config.default.js
@@ -4,15 +4,15 @@ module.exports = {
   // cors: {
   //   allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS',credentials: true,
   // },
-  // // 关闭csrf
-  // security: {
-  //   csrf: {
-  //     enable: false,
-  //     ignoreJSON: true,
-  //   },
-  //   // 白名单
-  //   domainWhiteList: [ 'http://localhost:9000' ],
-  // },
+  // 关闭csrf
+  security: {
+    csrf: {
+      enable: false,
+      ignoreJSON: true,
+    },
+    // 白名单
+    domainWhiteList: [ 'http://localhost:9000' ],
+  },
   // 配置需要的中间件，数组顺序即为中间件的加载顺序
   middleware: [ 'dealRes' ],
 }
