2d5a030899ab80c55d738d0edace250281a25752
runtu <huorunquan@126.com>
2018-10-15 13:46:46 +0800

    IMP: 增加统一的错误捕抓

--- config/config.default.js
+++ config/config.default.js
@@ -1,6 +1,29 @@
+// an accept detect function that mark all request with `x-requested-with=XMLHttpRequest` header accepts json.
+function accepts(ctx) {
+  if (ctx.get('x-requested-with') === 'XMLHttpRequest') return 'json';
+  return 'html';
+}
+
+// customize error handler, if all present, negotiation will be ignored.
+function all(e, ctx) {
+  ctx.body = {
+    status: 'error',
+    code: 500,
+    data: e.message,
+    stack: e.stack,
+  }
+  ctx.status = 500
+}
+
 module.exports = {
   keys: 'sddads',
   port: 3002,
+  // errorPageUrl support funtion
+  onerror: {
+    errorPageUrl: (err, ctx) => ctx.errorPageUrl || '/500',
+    accepts,
+    all,
+  },
   // cors: {
   //   allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS',credentials: true,
   // },
